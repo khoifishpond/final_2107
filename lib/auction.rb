507c6be9f0d37db8e632c4828d3120996a23af2d
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-11 08:50:30 -0700

    Iteration 2

--- lib/auction.rb
+++ lib/auction.rb
@@ -14,4 +14,22 @@ class Auction
       item.name
     end
   end
+
+  def unpopular_items
+    @items.select do |item|
+      item.bids.empty?
+    end
+  end
+
+  def items_with_bids
+    @items.select do |item|
+      item.bids.empty? == false
+    end
+  end
+
+  def potential_revenue
+    items_with_bids.sum do |item|
+      item.current_high_bid
+    end
+  end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,11 +1,11 @@
 require './lib/item'
+require './lib/attendee'
 require 'rspec'
 
 describe Item do
   context 'Attributes' do
     before(:each) do
       @item1 = Item.new('Chalkware Piggy Bank')
-      @item2 = Item.new('Bamboo Picture Frame')
     end
     
     it 'exists' do
@@ -14,6 +14,32 @@ describe Item do
 
     it 'has a name' do
       expect(@item1.name).to eq('Chalkware Piggy Bank')
+    end
+  end
+
+  context 'Bids' do
+    before(:each) do
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    end
+
+    it 'starts with no bids' do
+      expect(@item1.bids).to eq({})
+    end
+
+    it 'can add bids' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+    end
+
+    it 'has a highest bid' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+
+      expect(@item1.current_high_bid).to eq(22)
     end
   end
 end
